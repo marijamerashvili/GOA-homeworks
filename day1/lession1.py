71d898ca0115e8118dbf0f40d1726e56d29dff2a
marijamerashvili <mariamjamerashvili935@gmail.com>
2024-10-11 21:24:46 +0400

    first commit

---
+++ day1/lession1.py
@@ -0,0 +1,104 @@
+from turtle import *
+
+#5 + 5 შეკრების ჯამი
+print(f"ჩვენი პირველი ჯამი და კოდი: {5+5}")
+print("ჩვენი პირველი ჯამი და კოდი")
+print(5+5)
+print(f"ჩვენი პირველი ჯამი და კოდი")
+print(f"{5+5}")
+
+#saxlis kvadrati
+speed(11)
+width(16)
+color("purple")
+
+forward(200)
+right(90)
+forward(200)
+right(90)
+forward(200)
+right(90)
+forward(200)
+
+#krisha
+
+color("green")
+
+begin_fill()
+
+
+right(50)
+forward(140)
+right(84)
+forward(125)
+
+end_fill()
+
+
+#karebi
+
+color("purple")
+right(45)
+forward(200)
+right(90)
+forward(60)
+right(90)
+color("yellow")
+
+begin_fill()
+
+forward(120)     #karis sigdze
+left(90)
+forward(60)
+left(90)
+forward(120)
+
+end_fill()
+
+#ფანჯრები გამიჭირდა
+
+
+
+color("purple")
+right(90)
+forward(80)
+right(90)
+forward(130)
+right(110)
+penup()
+goto(30, (-20))
+pendown()
+
+
+
+color("black")
+width(9)
+left(19)
+forward(150)
+right(90)
+forward(40)
+right(90)
+forward(150)
+right(90)
+forward(40)
+backward(20)
+right(90)
+forward(150)
+left(90)
+forward(20)
+left(90)
+forward(75)
+left(90)
+forward(40)
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
