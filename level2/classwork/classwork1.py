764e4915e3ed81e17ecee5e1174c160f908ba41c
mariamjamerashvili <mariamjamerashvili935@gmail.com>
2024-10-21 02:45:10 +0400

    new changes

---
+++ level2/classwork/classwork1.py
@@ -0,0 +1,24 @@
+
+
+#ჩემზე ინპორმაცია
+
+name="mariami"
+sirname="jamerashvili"
+age=12
+animal="dog"
+
+#დავპრინტო თვითვეული მადგანი
+
+print(name)
+print(sirname)
+print(age)
+print(animal)
+
+#მათემატიკური ოპერაციები
+
+print(5 + 5)
+print(5 * 3)
+print(10 - 5)
+print(10/2)
+
+
---
+++ level2/homework/homework1.py
@@ -0,0 +1,33 @@
+
+
+name="mariam"
+sirname="jamerashvili"
+age=12
+hoby="programing"
+cuntry="georgia"
+
+print(name)
+print(sirname)
+print(age)
+print(hoby)
+print(cuntry)
+
+#ცვლადის შექმნა და ერთმანეთზე დამატება
+
+toys=15
+clothes=21
+print(toys + clothes)
+
+#მათემატიკურ ოპერაციებზე 10 მაგალითი
+
+print(21 + 9)
+print(50 + 50)
+print(20 + 1)
+print(7 + 40)
+print(2 - 1)
+print(60 - 15)
+print(5 * 5)
+print(100 * 100)
+print(60/3)
+print(100/2)
+
