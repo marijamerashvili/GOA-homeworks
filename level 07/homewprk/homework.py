f4d34e5edf38594dd59ae63bb5b0c43de5832f23
mariamjamerashvili <mariamjamerashvili935@gmail.com>
2024-11-17 21:24:52 +0400

    new folders

---
+++ level 07/homewprk/homework.py
@@ -0,0 +1,20 @@
+
+
+print(" რ ე გ ი ს ტ რ ა ც ი ა 2")
+name=input(" შეიყვანეთ თქვენი სახელი:")
+username=input("შეიყვანეთ თქვენი გვარი")
+imail=input("ჩაწერეთ თქვენი imail-ი")
+password=input("შეიყვანეთ პაროლი")
+print("გამარჯობა"+ name+username)
+
+equestion=input("დავიწყოთ მათემატიკური ოპერაცია? :")
+
+print("მათემატიკური ოპერაცია")
+num1=int(input("აირჩიეთ პირველი რიცხვი: ") )
+num2=int(input("აირჩიეთ მეორე რიცხვი:") )
+print("შედეგები :")
+print( num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print("დასასრული")
