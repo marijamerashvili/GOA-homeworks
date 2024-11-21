ab6b068369e7e64f1ae5202813b8c4b59ab32145
mariamjamerashvili <mariamjamerashvili935@gmail.com>
2024-11-21 22:34:16 +0400

    new file

---
+++ level 01/homework/homework.py
@@ -0,0 +1,12 @@
+
+
+name ="mariami"
+sirname = "jamerashvili"
+age = "dont want to say it"
+academy = "goa"
+
+print(name)
+print(name + sirname)
+print(academy)
+
+print("i love " + str(academy) + " academy")
