5cfe76169522fa9dff2cb150af27c557268e7f71
mariamjamerashvili <mariamjamerashvili935@gmail.com>
2024-12-22 20:47:50 +0400

    new classwork

---
+++ level 11/classwork/classwork.py
@@ -0,0 +1,45 @@
+
+#დავალება 1
+
+#ნაკლებობებზე
+print(10>5)
+print(50>10)
+print(5>3)
+print(2>1)
+print(80>1)
+print(40>10)
+print(6>3)
+print(9>5)
+print(33>22)
+print(9>8)
+
+#მეტობა
+#არ არის ტოლი (!=)
+print(60!=70)
+print(70!=80)
+print(4!=5)
+print(2!=9)
+print(68!=7)
+print(76!=742)
+print(46!=97)
+print(75!=98)
+print(79!=92)
+print(39!=73)
+
+#ტოლია
+print(10==10)
+print(20==20)
+print(30==30)
+print(40==40)
+print(50==50)
+print(60==60)
+print(70==70)
+print(80==80)
+print(90==90)
+print(100==100)
+
+#დავალება 2
+
+x = "hello"
+y = "hello"
+print(x == y)
