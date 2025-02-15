5bd6039d2eece9e08ed61b82c3e2e5e76dba7bbe
mariamjamerashvili <mariamjamerashvili935@gmail.com>
2025-02-15 15:20:21 +0400

    new files

---
+++ level 18/classwork/classwork.py
@@ -0,0 +1,18 @@
+
+#დავალება 1
+for i in range(20, 41):
+    print(i)
+
+#დავალება 2
+
+print("შეიყვანეთ თქვენი სახელი:  ")
+x = input()
+
+for _ in range(15):
+    print(x)
+
+#დავალება 3
+for i in range(2, 21, 2):
+    print(i)
+
+
